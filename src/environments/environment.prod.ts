c2e5e6e1759f070f236d327c9d71d7c1da784c48
Humayun Azaz <humayun.azaz@gmail.com>
2019-09-28 23:25:54 +0500

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyBl7tpXIB4B0fjPfvKDl9QuNBZpXjJs7Yg",
+    
     authDomain: "carfoamandwash.firebaseapp.com",
     databaseURL: "https://carfoamandwash.firebaseio.com",
     projectId: "carfoamandwash",
